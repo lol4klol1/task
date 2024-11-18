b83af90bbfc468bbd14f101e40f7f13364c3bf20
ikirsul'ev <ikirsul'ev@DJ-PC-K115-05>
2024-11-18 16:39:22 +0300

    Добавьте файлы проекта.

---
+++ task/authorization.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task
+{ 
+    public partial class authorization : Form
+    {
+       
+        public authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            registration newForm = new registration();
+            newForm.Show();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
